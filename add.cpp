7d7e0e1970b1e95ee1bd5876c39084610bc7cc9a
poornaks314 <63178045+poornaks314@users.noreply.github.com>
2024-06-01 01:19:30 +0530

    added additional operation

--- add.cpp
+++ add.cpp
@@ -4,8 +4,23 @@ int add(int a, int b)
 {
 	return a+b;
 }
+int sub(int a, int b)
+{
+	return a-b;
+}
+int mul(int a, int b)
+{
+	return a*b;
+}
+int div_d(int a, int b)
+{
+	return a/b;
+}
 int main()
 {
-	cout<<"sum:" << add(10,20)<<endl;
+	cout<<"result: " << add(10,20)<<endl;
+	cout<<"result: " << sub(10,20)<<endl;
+	cout<<"result: " << mul(10,20)<<endl;
+	cout<<"result: " << div_d(10,20)<<endl;
 	return 0;
 }
